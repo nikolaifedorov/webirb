d7fe3ddbc30558c8b388c85122005aed80923196
nfedorov <nfedorov@fiksu.com>
2013-04-16 21:37:46 +0400

    added execute in subprocess.

--- lib/ruby_executer.rb
+++ lib/ruby_executer.rb
@@ -1,10 +1,42 @@
+$SAFE = 2
+
 class RubyExecuter
 
   attr_reader :output, :exception, :result
 
 
   def execute(code)
-    @output = capture_stdout { @result = eval(code) }
+
+    reader, writer = IO.pipe
+
+    pid = fork do
+      begin
+        @output = capture_stdout { @result = eval(code) }
+      rescue Exception => ex
+        @exception = ex
+      ensure
+        reader.close
+        writer.write @output
+        writer.write "-|-"
+        writer.write @exception
+        writer.write "-|-"
+        writer.write @result
+      end
+    end
+  
+    status = Timeout::timeout(20) {
+      Process.wait(pid)
+    }
+
+    writer.close
+    data = reader.read.split("-|-")
+    @output = data[0]
+    @exception = data[1]
+    @result = data[2]
+
+  rescue Timeout::Error => ex
+    Process.kill('KILL', pid)
+    @exception = ex
   rescue Exception => ex
     @exception = ex
   end
